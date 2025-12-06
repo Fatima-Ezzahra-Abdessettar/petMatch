eec009d897ea7f2560140116cebd93107c8592b9
ghizlane-aspic <ghizlanetouguii@gmail.com>
2025-12-06 13:06:54 +0100

    commit to pull

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,21 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://backend.test',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL ,
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
-  }
+  };
 });
-
